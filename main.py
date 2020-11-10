b59d47ecd9507b7ba2ddca3247c795ef3621722f
MYR <loic.themyr@gmail.com>
2020-11-10 16:28:45 +0100

    debug

--- main.py
+++ main.py
@@ -57,9 +57,9 @@ def pick_an_image(path, n = 1, frame = 120, save_path = 'image'):
 
 	for key in list(im_dict.keys()):
 
-		img1 = utils.nibfile(im_dict[key])[frame, :, :]
-		img2 = utils.nibfile(im_dict[key])[:, frame, :]
-		img3 = utils.nibfile(im_dict[key])[:, :, frame]
+		img1 = utils.nibfile(im_dict[key][n])[frame, :, :]
+		img2 = utils.nibfile(im_dict[key][n])[:, frame, :]
+		img3 = utils.nibfile(im_dict[key][n])[:, :, frame]
 
 		utils.save_image(img1, save_path+str(n)+'_'+key[:2]+'_'+str(frame)+'_1_.pny')
 		utils.save_image(img2, save_path+str(n)+'_'+key[:2]+'_'+str(frame)+'_2_.pny')
